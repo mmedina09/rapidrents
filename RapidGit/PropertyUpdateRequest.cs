936c193c4cf88fe287af8e6b60b3eff90bbd05e5
mmedina09 <medain.mark@gmail.com>
2016-09-23 15:44:06 -0700

    Update PropertyUpdateRequest.cs

--- RapidGit/PropertyUpdateRequest.cs
+++ RapidGit/PropertyUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Models.Requests.Properties
+namespace Rapid.Web.Models.Requests.Properties
 {
     public class PropertyUpdateRequest : PropertyAddRequest
     {
